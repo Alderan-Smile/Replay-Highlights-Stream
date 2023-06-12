d7b09f0351d83d9ecf9df2250977644382c871e8
Oliver Consterla Araya <oliver_consterla@yahoo.cl>
2023-06-12 18:10:04 -0400

    Cambio de cv2 a moviepy/pyglet/pydub

--- newReplay.py
+++ newReplay.py
@@ -1,62 +1,95 @@
-import os
 import configparser
-import keyboard
+import os
 import time
+import keyboard
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from moviepy.editor import VideoFileClip
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import pyglet
+from pydub import AudioSegment
+from pydub.playback import play as play_audio
 
+# Lee la configuración desde el archivo externo config.ini
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-video_folder = config['Paths']['video_folder']
-temp_folder = config['Paths']['temp_folder']
+# Rutas de carpetas
+watch_folder = config.get('Paths', 'WatchFolder')
+ignore_folder = config.get('Paths', 'IgnoreFolder')
 
+# Lista de reproducción
 playlist = []
-played_videos = []
+played_videos = set()
 
-
-class FileEventHandler(FileSystemEventHandler):
+# Clase de manipulador de eventos para detectar cambios en la carpeta
+class FileCreatedEventHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory and event.src_path.endswith('.mp4'):
-            if event.src_path.startswith(temp_folder):
-                return
+            video_path = event.src_path
+            if video_path not in playlist and not video_path.startswith(ignore_folder):
+                playlist.append(video_path)
 
-            if event.src_path not in playlist:
-                playlist.append(event.src_path)
+# Función para reproducir los videos en la lista de reproducción
+def play_videos():
+    while True:
+        if len(playlist) == 0:
+            time.sleep(1)
+            continue
 
-            if len(playlist) == 1:
-                keyboard.press_and_release('page up')
+        video_path = playlist.pop(0)
+        if video_path not in played_videos:
+            print(f'Reproduciendo: {video_path}')
+
+            # Simula la tecla "page up" antes de iniciar la reproducción
+            keyboard.press('page up')
+            keyboard.release('page up')
+
+            # Extrae el audio del video utilizando pydub
+            video = VideoFileClip(video_path)
+            audio = video.audio
+            audio_path = 'temp_audio.wav'
+            audio.write_audiofile(audio_path, codec='pcm_s16le')
+
+            # Reproduce el audio con pyglet
+            audio_player = pyglet.media.Player()
+            audio = pyglet.media.load(audio_path)
+            audio_player.queue(audio)
+            audio_player.play()
+
+            # Registra el evento de finalización de reproducción del audio
+            @audio_player.event
+            def on_player_eos():
+                audio_player.delete()
+                os.remove(audio_path)
+
+            # Muestra el video sin audio utilizando MoviePy
+            video.preview(fullscreen=False, audio=False)
+            video.close()
+
+            played_videos.add(video_path)
+            print(f'Terminado: {video_path}')
+
+            # Simula la tecla "page down" después de reproducir el último video
+            if len(playlist) == 0:
+                time.sleep(2)
+                keyboard.press('page down')
+                keyboard.release('page down')
+
+            # Elimina el video de la lista de reproducción
+            if video_path in played_videos:
+                played_videos.remove(video_path)
 
 
-def play_video(file_path):
-    video = VideoFileClip(file_path)
-    video.preview()
-    played_videos.append(file_path)
-    playlist.remove(file_path)
-    keyboard.press_and_release('page down')
+# Configura el observador para detectar cambios en la carpeta
+event_handler = FileCreatedEventHandler()
+observer = Observer()
+observer.schedule(event_handler, watch_folder, recursive=True)
+observer.start()
 
+# Inicia la reproducción de videos
+play_videos()
 
-def start_monitoring():
-    event_handler = FileEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, video_folder, recursive=True)
-    observer.start()
-
-    try:
-        while True:
-            if len(playlist) > 0:
-                file_path = playlist[0]
-                if file_path not in played_videos:
-                    play_video(file_path)
-            else:
-                time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-
-    observer.join()
-
-
-if __name__ == '__main__':
-    start_monitoring()
+# Detiene el observador cuando se cierra la ventana
+keyboard.wait('esc')
+observer.stop()
+observer.join()
